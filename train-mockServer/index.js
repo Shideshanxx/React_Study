6d4d809254c5bbb6d912c947db1142df355b7c04
ShiDeshan <307042873@qq.com>
2020-06-02 15:19:37 +0800

    开发服务端接口

--- train-mockServer/index.js
+++ train-mockServer/index.js
@@ -1,18 +1,8 @@
 const express = require('express');
+const path = require('path');
+const apiMocker = require('mocker-api');
 
 const app = express();
 
-app.get('/', (request,response) => {
-    response.status(200);
-    response.send('hello express');
-    response.end()
-})
-
-app.get('/rest', (request, response) => {
-    response.json({
-        result: 1,
-        msg: 'hello express'
-    });
-});
-
-app.listen(5000)
+apiMocker(app, path.resolve('./mocker/mocker.js'))
+app.listen(80);
