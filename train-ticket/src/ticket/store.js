68261da0a9624d90cc478adec5f068d3a3e1e8f8
ShiDeshan <307042873@qq.com>
2020-06-09 06:57:42 +0800

    车票详情页完成

--- train-ticket/src/ticket/store.js
+++ train-ticket/src/ticket/store.js
@@ -10,7 +10,17 @@ import thunk from 'redux-thunk';
 const store = createStore(
     combineReducers(reducers),
     {
-        
+        departDate: Date.now(),
+        arriveDate: Date.now(),
+        departTimeStr: null,
+        arriveTimeStr: null,
+        departStation: null,
+        arriveStation: null,
+        trainNumber: null,
+        durationStr: null,
+        tickets: [],
+        isScheduleVisible: false,
+        searchParsed: false
     },
     applyMiddleware(thunk)
 )
